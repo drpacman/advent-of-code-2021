ce360a96014dc97e2283695ccaef08393032da32
Paul Caporn <paul.caporn@bbc.co.uk>
2021-12-01 22:31:10 +0000

    Remove run

--- app/Main.hs
+++ app/Main.hs
@@ -4,14 +4,9 @@ import qualified Day1 (parts)
 import Utils(PuzzlePart)
 import Text.Printf
 
-run :: (Show a) => String -> Int -> PuzzlePart a -> IO ()
-run filename n part =  
-  do
-    contents <- readFile ("inputs/" ++ filename)    
-    printf "Part %d => %s\n" n $ show (part (lines contents))
-
 main :: IO ()
 main = do
-  run "day1.txt" 1 part1
-  run "day1.txt" 2 part2
+  contents <- readFile ("inputs/day1.txt")    
+  printf "Part 1 => %s\n" $ show (part1 (lines contents))
+  printf "Part 2 => %s\n" $ show (part2 (lines contents))
   where (part1, part2) = Day1.parts
