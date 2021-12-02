d4563f0780b941c7c7ae9a7beb3946d0946662cc
Paul Caporn <paul.caporn@bbc.co.uk>
2021-12-02 08:32:50 +0000

    Day 2

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,11 @@
 module Main where
 
-import qualified Day1 (parts)
+import Day2
 import Utils(PuzzlePart)
 import Text.Printf
 
 main :: IO ()
 main = do
-  contents <- readFile ("inputs/day1.txt")    
+  contents <- readFile ("inputs/day2.txt")    
   printf "Part 1 => %s\n" $ show (part1 (lines contents))
   printf "Part 2 => %s\n" $ show (part2 (lines contents))
-  where (part1, part2) = Day1.parts
---
+++ src/Day2.hs
@@ -0,0 +1,45 @@
+module Day2(part1,part2) where
+
+import Utils(PuzzlePart)
+import Debug.Trace
+
+data Direction = Forward Int 
+               | Up Int
+               | Down Int deriving (Show)
+
+toDirection :: String -> Direction
+toDirection s = 
+    case words s of
+        "forward":x:[] -> Forward (read x::Int)
+        "up":x:[] -> Up (read x::Int)
+        "down":x:[] -> Down (read x::Int)
+        otherwise -> trace(show otherwise) error "Unexpected direction"
+
+calculatePostionPart1 :: [Direction] -> (Int, Int)
+calculatePostionPart1 directions = 
+    foldl (\(x,y) direction -> 
+            case direction of
+                Forward n -> (x+n,y)
+                Up n -> (x, y-n)
+                Down n -> (x, y+n)
+        ) (0,0) directions
+
+calculatePostionPart2 :: [Direction] -> (Int, Int, Int)
+calculatePostionPart2 directions = 
+    foldl (\(x,y,aim) direction -> 
+            case direction of
+                Forward n -> (x+n,y+(aim * n), aim)
+                Up n -> (x, y, aim-n)
+                Down n -> (x, y, aim+n)
+        ) (0,0,0) directions
+
+part1 :: PuzzlePart Int
+part1 input = 
+    x * y    
+    where (x,y) = calculatePostionPart1 (map toDirection input)
+
+part2 :: PuzzlePart Int
+part2 input = 
+    x * y    
+    where (x,y,_) = calculatePostionPart2 (map toDirection input)
+        
