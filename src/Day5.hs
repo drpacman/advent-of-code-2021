fe5feefede6129c73729f1338229c6a2c3069320
Paul Caporn <paul.caporn@bbc.co.uk>
2021-12-05 23:07:40 +0000

    Refactor to reduce duplication and use TupleSections

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day5(part1, part2) where
 import Utils (PuzzlePart)
 import Data.List
@@ -8,21 +10,21 @@ type Coord = (Int, Int)
 type Vent = (Coord, Coord)
 
 toVents:: [String] -> [Vent]
-toVents input =  map toVent input
+toVents =  map toVent
 
 toVent:: String -> Vent 
 toVent input = ((startX, startY), (endX, endY))
     where
         entries = words input
-        startX:startY:[] = map read (splitOn "," (head entries))
-        endX:endY:[] = map read (splitOn "," (last entries))
+        [startX,startY] = map read (splitOn "," (head entries))
+        [endX,endY] = map read (splitOn "," (last entries))
 
 straightEntries :: Vent -> [Coord]
 straightEntries ((x1,y1),(x2,y2)) 
-  | x1 == x2 && y1 > y2 = map (\y -> (x1, y)) [y2..y1]
-  | x1 == x2 && y1 < y2 = map (\y -> (x1, y)) [y1..y2]
-  | x1 > x2 && y1 == y2 = map (\x -> (x, y1)) [x2..x1]
-  | x1 < x2 && y1 == y2 = map (\x -> (x, y1)) [x1..x2]
+  | x1 == x2 && y1 > y2 = map (x1,) [y2..y1]
+  | x1 == x2 && y1 < y2 = map (x1,) [y1..y2]
+  | x1 > x2 && y1 == y2 = map (,y1) [x2..x1]
+  | x1 < x2 && y1 == y2 = map (,y1) [x1..x2]
   | otherwise = []
 
 diagonalEntries :: Vent -> [Coord]
@@ -33,17 +35,15 @@ diagonalEntries ((x1,y1),(x2,y2))
   | x1 < x2 && y1 < y2 = zip [x1..x2] [y1..y2]
   | otherwise = []
 
-part1 :: PuzzlePart Int 
-part1 input = length $ filter (\c -> length c > 1) allCoords
+countDuplicates :: [String] -> [ Vent -> [Coord] ] -> Int
+countDuplicates input entries = length $ filter (\c -> length c > 1) allCoords
     where 
         vents = toVents input
-        straight = filter (/= []) $ map straightEntries vents
-        allCoords = group . sort $ concat straight
+        expandedVents = filter (/= []) $ foldl (\acc f -> acc ++ map f vents) [] entries
+        allCoords = group . sort $ concat expandedVents
+
+part1 :: PuzzlePart Int 
+part1 input = countDuplicates input [straightEntries]
 
 part2 :: PuzzlePart Int 
-part2 input = length $ filter (\c -> length c > 1) allCoords
-    where 
-        vents = toVents input
-        straight = filter (/= []) $ map straightEntries vents
-        diagonal = filter (/= []) $ map diagonalEntries vents
-        allCoords = group . sort $ concat straight ++ concat diagonal
+part2 input = countDuplicates input [straightEntries, diagonalEntries]
