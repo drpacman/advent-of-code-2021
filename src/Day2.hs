93726d591c21769fd414086e5d1378312f25d5e1
Paul Caporn <paul.caporn@bbc.co.uk>
2021-12-05 23:24:03 +0000

    Tidy code

--- src/Day2.hs
+++ src/Day2.hs
@@ -10,28 +10,28 @@ data Direction = Forward Int
 toDirection :: String -> Direction
 toDirection s = 
     case words s of
-        "forward":x:[] -> Forward (read x::Int)
-        "up":x:[] -> Up (read x::Int)
-        "down":x:[] -> Down (read x::Int)
+        ["forward",x] -> Forward (read x::Int)
+        ["up",x] -> Up (read x::Int)
+        ["down",x] -> Down (read x::Int)
         otherwise -> trace(show otherwise) error "Unexpected direction"
 
 calculatePostionPart1 :: [Direction] -> (Int, Int)
-calculatePostionPart1 directions = 
+calculatePostionPart1 = 
     foldl (\(x,y) direction -> 
             case direction of
                 Forward n -> (x+n,y)
                 Up n -> (x, y-n)
                 Down n -> (x, y+n)
-        ) (0,0) directions
+        ) (0,0)
 
 calculatePostionPart2 :: [Direction] -> (Int, Int, Int)
-calculatePostionPart2 directions = 
+calculatePostionPart2 = 
     foldl (\(x,y,aim) direction -> 
             case direction of
                 Forward n -> (x+n,y+(aim * n), aim)
                 Up n -> (x, y, aim-n)
                 Down n -> (x, y, aim+n)
-        ) (0,0,0) directions
+        ) (0,0,0)
 
 part1 :: PuzzlePart Int
 part1 input = 
