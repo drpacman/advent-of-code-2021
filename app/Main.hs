46979333f2a7d6ca912cb2771a9d88d98a0ac29c
Paul Caporn <paul.caporn@bbc.co.uk>
2021-12-06 23:02:12 +0000

    Day 6 using Data.Map

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,11 @@
 module Main where
 
-import Day5
+import Day6
 import Utils(PuzzlePart)
 import Text.Printf
 
 main :: IO ()
 main = do
-  contents <- readFile ("inputs/day5.txt")    
+  contents <- readFile ("inputs/day6.txt")    
   printf "Part 1 => %s\n" $ show (part1 (lines contents))
   printf "Part 2 => %s\n" $ show (part2 (lines contents))
---
+++ src/Day6.hs
@@ -0,0 +1,34 @@
+module Day6(part1, part2) where
+import Utils (PuzzlePart)
+
+import Data.List.Split
+import qualified Data.List (map, group, sort)
+import Debug.Trace
+import Data.Map 
+
+runRound :: Map Int Int -> Map Int Int
+runRound entries = if Data.Map.member 6 withNewEntries 
+                     then 
+                        Data.Map.adjust (+newCount) 6 withNewEntries
+                    else
+                        Data.Map.insert 6 newCount withNewEntries
+                    where 
+                        newCount = findWithDefault 0 0 entries 
+                        droppedZeros = delete 0 entries
+                        -- decrement the entries
+                        existing = Data.Map.mapKeys (\x -> x-1) droppedZeros 
+                        -- add new entries
+                        withNewEntries = Data.Map.insert 8 newCount existing
+
+calculate :: [String] -> Int -> Int
+calculate input n = Data.Map.foldl (+) 0 result
+    where 
+        -- create Map of value -> count
+        seed = Data.List.map (\x -> (head x, length x)) $ Data.List.group $ Data.List.sort $ Data.List.map read (splitOn "," (head input))
+        result = iterate runRound (fromList seed) !! n
+
+part1 :: PuzzlePart Int 
+part1 input = calculate input 80
+
+part2 :: PuzzlePart Int 
+part2 input = calculate input 256
