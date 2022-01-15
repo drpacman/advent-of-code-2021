95bf5f6b2f0a07e55144c384f8f8a02bc5c71441
Paul Caporn <paul.caporn@bbc.co.uk>
2022-01-15 17:13:17 +0000

    Clean up

--- src/Day19.hs
+++ src/Day19.hs
@@ -32,31 +32,31 @@ transforms = [ v.r | v <- variations, r <- rotations ]
 
 -- shift co-ordinates to be relative to a specific beacon
 shiftToOffset :: Coord -> [Coord] -> [Coord]
-shiftToOffset (x,y,z) coords = map (\(x1, y1, z1) -> (x - x1, y - y1, z - z1)) coords
+shiftToOffset (x,y,z) = map (\(x1, y1, z1) -> (x - x1, y - y1, z - z1))
 
 
 toScans :: [String] -> [Coord] -> [[Coord]] -> [[Coord]]
-toScans [] cs scans = reverse ((reverse cs):scans)
+toScans [] cs scans = reverse (reverse cs:scans)
 toScans input cs scans = 
     if null (head input) then
         -- start next scan
-        toScans (drop 2 input) [] ((reverse cs):scans)
+        toScans (drop 2 input) [] (reverse cs:scans)
     else 
         -- add next coord to current scan
-        toScans (tail input) ((toCoord (head input)):cs) scans
+        toScans (tail input) (toCoord (head input):cs) scans
 
 toCoord :: String -> Coord
 toCoord coords = (read x, read y, read z)
     where
         (x,y,z) = case wordsBy (==',') coords of 
-                        (x:y:z:[]) -> (x,y,z)
+                        [x,y,z] -> (x,y,z)
                         w -> error (show w)
 
 -- shift co-ordinates to be relative to a specific beacon for both the target (base) and the candidate
 -- if any combination has 12 or more of the same entries we have found a match
 match :: [Coord] -> [Coord] -> Maybe (Coord, Coord)
 match candidate base = 
-    if length matchedBeacons > 0 
+    if null matchedBeacons
         then
             head matchedBeacons
         else 
@@ -66,12 +66,12 @@ match candidate base =
         shiftedBasesWithBeacon = map (\beacon -> (beacon, shiftToOffset beacon base)) base
         beaconWithIntersections = map (\(candidateBeacon, c) -> 
                             map (\(baseBeacon, b) -> 
-                                ((baseBeacon, candidateBeacon), Data.List.intersect b c)
+                                ((baseBeacon, candidateBeacon), b `Data.List.intersect` c)
                             ) shiftedBasesWithBeacon
                         ) shiftedCandidatesWithBeacon
         matchedBeacons = filter isJust $ map (\xs -> 
                             let 
-                                matchingBeacons = find (\((b1,b2),shared)-> (length shared) >= 12) xs 
+                                matchingBeacons = find (\((b1,b2),shared)-> length shared >= 12) xs 
                             in
                                 case matchingBeacons of
                                     Just ((b1,b2), _) ->
@@ -136,13 +136,13 @@ part1 input = trace (show beacons) length beacons
     where
         scans = toScans (tail input) [] []
         scan0 = head scans
-        (beacons, _) = resolveScans ((tail scans)) [scan0] [(scan0, (0,0,0))]
+        (beacons, _) = resolveScans (tail scans) [scan0] [(scan0, (0,0,0))]
 
 part2 :: PuzzlePart Int
 part2 input = maximum differences
     where
         scans = toScans (tail input) [] []
         scan0 = head scans
-        (_, offsets) = resolveScans ((tail scans)) [scan0] [(scan0, (0,0,0))]
+        (_, offsets) = resolveScans (tail scans) [scan0] [(scan0, (0,0,0))]
         scannerPairs = [ (a,b) | a <- offsets, b <- offsets ]
-        differences = map (\((x1,y1,z1),(x2,y2,z2)) -> (abs(x1-x2) + abs(y1-y2) + abs(z1-z2))) scannerPairs
+        differences = map (\((x1,y1,z1),(x2,y2,z2)) -> abs(x1-x2) + abs(y1-y2) + abs(z1-z2)) scannerPairs
